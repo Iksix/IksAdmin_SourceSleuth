209040aff59ed6b5534e7318fd34072ca350b94e
iksixx <pocetikyt@gmail.com>
2024-04-27 19:21:24 +0700

    Start

---
+++ Main.cs
@@ -0,0 +1,12 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace IksAdmin_SourceSleuth;
+
+public class Main : BasePlugin
+{
+    public override string ModuleName => "IksAdmin_SourceSleuth";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "iks__";
+    
+    
+}
---
+++ PluginConfig.cs
@@ -0,0 +1,9 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace IksAdmin_SourceSleuth;
+
+public class PluginConfig : BasePluginConfig
+{
+    public bool BanNewPlayerAccount { get; set; } = false;
+    public bool NotifyAdminsAboutBans { get; set; } = true;
+}
